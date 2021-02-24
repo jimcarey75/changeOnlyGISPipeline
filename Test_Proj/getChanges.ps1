d901df9df41f531ba04544dac5c550588dc27c9e
jimcarey75 <jim.carey@landmark.co.uk>
2021-02-24 14:40:50 +0000

    add ps1

---
+++ Test_Proj/getChanges.ps1
@@ -0,0 +1,16 @@
+$buildNumber="%system.build.number%"
+$change_file="%system.teamcity.build.changedFiles.file%"
+$deployPackagesFile="packages_to_deploy.txt"
+
+if (Test-Path $deployPackagesFile) {
+	Remove-Item $deployPackagesFile
+}
+$change_contents = Get-Content $change_file
+#$change_contents >> changeContents.txt
+
+$change_contents | %{
+	$file = $_.Split(":")[0]
+	$file >> changeContents.txt
+	$file_extension = $file.Split(".")[-1]
+	$file_extension >> changeContents.txt
+    }
